35775faa85eaca6b863f9b828477c81e4926bb24
Tom ten Thij <git@tomtenthij.nl>
2008-12-29 18:25:13 +0000

    Basic layout based on 960 grid system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def show_flashes
+    return '' unless flash.length > 0
+    output = "<div id=\"flashes\" style=\"display:inherit;\">\n"
+    flash.each do |key, value|
+      output += content_tag('div', value.to_s, :class => "flash_#{key.to_s}")
+    end
+    output += "</div>\n"
+    output
+  end
 end
