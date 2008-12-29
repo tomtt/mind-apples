72dc8b83163bb2acaf5518ab979132774617568c
Tom ten Thij <git@tomtenthij.nl>
2008-12-29 01:47:52 +0000

    Factory girl file created with defs for user

---
+++ spec/factory_girl_defs.rb
@@ -0,0 +1,13 @@
+require 'factory_girl'
+
+Factory.sequence :email_address do |n|
+  "somebody#{n}@example.com"
+end
+
+Factory.define :user do |f|
+  f.name                    'Jane Doe'
+  f.login                   'jdoe'
+  f.password                'doe_a_deer'
+  f.password_confirmation   'doe_a_deer'
+  f.email_address           { Factory.next(:email_address) }
+end
