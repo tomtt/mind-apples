4df110a6c6755f6577a585e521fafb0323388366
Tom ten Thij <git@tomtenthij.nl>
2009-01-02 19:52:39 +0000

    Fixed view specs

--- spec/views/surveys/new.html.erb_spec.rb
+++ spec/views/surveys/new.html.erb_spec.rb
@@ -24,14 +24,14 @@ describe "/surveys/new.html.erb" do
     render "/surveys/new.html.erb"
     
     response.should have_tag("form[action=?][method=post]", surveys_path) do
-      with_tag("textarea#survey_apple_1[name=?]", "survey[apple_1]")
-      with_tag("textarea#survey_apple_2[name=?]", "survey[apple_2]")
-      with_tag("textarea#survey_apple_3[name=?]", "survey[apple_3]")
-      with_tag("textarea#survey_apple_4[name=?]", "survey[apple_4]")
-      with_tag("textarea#survey_apple_5[name=?]", "survey[apple_5]")
+      with_tag("input#survey_apple_1[name=?]", "survey[apple_1]")
+      with_tag("input#survey_apple_2[name=?]", "survey[apple_2]")
+      with_tag("input#survey_apple_3[name=?]", "survey[apple_3]")
+      with_tag("input#survey_apple_4[name=?]", "survey[apple_4]")
+      with_tag("input#survey_apple_5[name=?]", "survey[apple_5]")
       with_tag("input#survey_health_check[name=?]", "survey[health_check]")
-      with_tag("textarea#survey_famous_fives[name=?]", "survey[famous_fives]")
-      with_tag("input#survey_age_range[name=?]", "survey[age_range]")
+      with_tag("input#survey_famous_fives[name=?]", "survey[famous_fives]")
+      with_tag("select#survey_age_range[name=?]", "survey[age_range]")
       with_tag("input#survey_country[name=?]", "survey[country]")
       with_tag("input#survey_name[name=?]", "survey[name]")
       with_tag("input#survey_email[name=?]", "survey[email]")
