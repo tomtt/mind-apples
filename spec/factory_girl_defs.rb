07efa15fc54c1344afda066bbeafea6042a2b932
Tom ten Thij <git@tomtenthij.nl>
2009-02-17 15:42:06 +0000

    Factory definitions for survey

--- spec/factory_girl_defs.rb
+++ spec/factory_girl_defs.rb
@@ -11,3 +11,17 @@ Factory.define :user do |f|
   f.password_confirmation   'doe_a_deer'
   f.email                   { Factory.next(:email_address) }
 end
+
+Factory.define :survey do |f|
+  f.apple_1 "value for apple_1"
+  f.apple_2 "value for apple_2"
+  f.apple_3 "value for apple_3"
+  f.apple_4 "value for apple_4"
+  f.apple_5 "value for apple_5"
+  f.health_check "4"
+  f.famous_fives "value for famous_fives"
+  f.age_range "value for age_range"
+  f.country "value for country"
+  f.name "value for name"
+  f.email "value for email"
+end
