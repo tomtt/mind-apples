95bc0d9d578fa1b9a977af61376ed783f31e3e58
Tom ten Thij <git@tomtenthij.nl>
2009-02-17 16:06:42 +0000

    Survey is assigned a random private_auth string on creation

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,9 @@
 class Survey < ActiveRecord::Base
+  before_validation_on_create :create_auth_code
+
+  private
+
+  def create_auth_code
+    self.private_auth = RandomString.generate
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,23 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Survey do
-  before(:each) do
-    @valid_attributes = {
-      :apple_1 => "value for apple_1",
-      :apple_2 => "value for apple_2",
-      :apple_3 => "value for apple_3",
-      :apple_4 => "value for apple_4",
-      :apple_5 => "value for apple_5",
-      :health_check => "1",
-      :famous_fives => "value for famous_fives",
-      :age_range => "value for age_range",
-      :country => "value for country",
-      :name => "value for name",
-      :email => "value for email"
-    }
+  it "should create a new instance given valid attributes" do
+    Factory.build(:survey).should be_valid
   end
 
-  it "should create a new instance given valid attributes" do
-    Survey.create!(@valid_attributes)
+  it "should assign a private auth code to a created survey" do
+    @survey = Factory.create(:survey)
+    @survey.private_auth.should_not be_nil
   end
 end
