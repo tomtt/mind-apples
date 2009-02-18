75d5953271d680336cf6f475e2116d51fbfbf056
Tom ten Thij <git@tomtenthij.nl>
2009-02-18 15:57:17 +0000

    Increase number of characters in random string

--- lib/random_string.rb
+++ lib/random_string.rb
@@ -1,7 +1,7 @@
 class RandomString
   @@possible_chars = [('a'..'z'),('A'..'Z'),('0'..'9')].map{|i| i.to_a}.flatten;
 
-  def self.generate(size = 10)
+  def self.generate(size = 20)
     (0...size).map { @@possible_chars[rand(@@possible_chars.length)]  }.join
   end
 end
