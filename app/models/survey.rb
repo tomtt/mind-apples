88eb97899b760ad7a01c36c4cbd55fd0d63da660
Tom ten Thij <git@tomtenthij.nl>
2009-02-20 15:48:16 +0000

    Named scope for surveys containing a phrase

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,11 @@
 class Survey < ActiveRecord::Base
   before_validation_on_create :create_auth_code
 
+  named_scope :containing_phrase, lambda{ |phrase|
+    search_condition = (1..5).map { |i| "apple_#{i} like ?" }.join(" or ")
+    { :conditions => [search_condition] + (1..5).map { "%#{phrase}%" } }
+  }
+
   private
 
   def create_auth_code
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -9,4 +9,28 @@ describe Survey do
     @survey = Factory.create(:survey)
     @survey.private_auth.should_not be_nil
   end
+
+  describe "searching in responses" do
+    it "does not find any surveys if none contain the phrase" do
+      s1 = Factory.create(:survey, :apple_1 => "stroke something furry")
+      s2 = Factory.create(:survey, :apple_2 => "do something creative")
+      s3 = Factory.create(:survey, :apple_3 => "make something tasty")
+      s4 = Factory.create(:survey, :apple_4 => "help create something")
+      s5 = Factory.create(:survey, :apple_5 => "something lovely")
+      Survey.containing_phrase("dance").should be_empty
+    end
+
+    it "finds any survey that contains the phrase" do
+      s1 = Factory.create(:survey, :apple_1 => "stroke something furry")
+      s2 = Factory.create(:survey, :apple_2 => "do something creative")
+      s3 = Factory.create(:survey, :apple_3 => "make something tasty")
+      s4 = Factory.create(:survey, :apple_4 => "help create something")
+      s5 = Factory.create(:survey, :apple_5 => "something lovely")
+      Survey.containing_phrase("something").should include(s1)
+      Survey.containing_phrase("something").should include(s2)
+      Survey.containing_phrase("something").should include(s3)
+      Survey.containing_phrase("something").should include(s4)
+      Survey.containing_phrase("something").should include(s5)
+    end
+  end
 end
