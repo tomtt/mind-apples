2fb1c69a8459da947fc6233f63cbe33ce1d6c8ae
Tom ten Thij <git@tomtenthij.nl>
2009-07-28 15:17:55 +0100

    About page

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,7 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "pages", :action => "about"
 
   # See how all your routes lay out with "rake routes"
 
---
+++ features/about_page.feature
@@ -0,0 +1,8 @@
+Feature: About page
+  In order to explain why Mindapples
+  As the evil overlord
+  I want there to be a page with some blurb
+
+  Scenario: Front page place holder
+    When I go to the homepage
+    Then I should see "About Mindapples"
