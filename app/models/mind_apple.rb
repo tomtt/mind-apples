1f7d8b46763860f23abb35af575b9bc2f1af19de
Tom ten Thij <git@tomtenthij.nl>
2009-07-29 13:54:49 +0100

    generate rspec_model mind_apple suggestion:text respondent:references

---
+++ app/models/mind_apple.rb
@@ -0,0 +1,3 @@
+class MindApple < ActiveRecord::Base
+  belongs_to :respondent
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090729125310) do
+
+  create_table "mind_apples", :force => true do |t|
+    t.text     "suggestion"
+    t.integer  "respondent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
