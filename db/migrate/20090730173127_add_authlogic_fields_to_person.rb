a5dd1699e3418d4ada69b03f9a97420aca76dd6d
Tom ten Thij <git@tomtenthij.nl>
2009-07-30 18:50:58 +0100

    Add authlogic fields to person

---
+++ db/migrate/20090730173127_add_authlogic_fields_to_person.rb
@@ -0,0 +1,33 @@
+class AddAuthlogicFieldsToPerson < ActiveRecord::Migration
+  def self.up
+    add_column :people, :login, :string, :null => false
+    add_column :people, :crypted_password, :string, :null => false
+    add_column :people, :password_salt, :string, :null => false
+    add_column :people, :persistence_token, :string, :null => false
+    add_column :people, :single_access_token, :string, :null => false
+    add_column :people, :perishable_token, :string, :null => false
+    add_column :people, :login_count, :integer, :null => false, :default => 0
+    add_column :people, :failed_login_count, :integer, :null => false, :default => 0
+    add_column :people, :last_request_at, :datetime
+    add_column :people, :current_login_at, :datetime
+    add_column :people, :last_login_at, :datetime
+    add_column :people, :current_login_ip, :string
+    add_column :people, :last_login_ip, :string
+  end
+
+  def self.down
+    remove_column :people, :last_login_ip
+    remove_column :people, :current_login_ip
+    remove_column :people, :last_login_at
+    remove_column :people, :current_login_at
+    remove_column :people, :last_request_at
+    remove_column :people, :failed_login_count
+    remove_column :people, :login_count
+    remove_column :people, :perishable_token
+    remove_column :people, :single_access_token
+    remove_column :people, :persistence_token
+    remove_column :people, :password_salt
+    remove_column :people, :crypted_password
+    remove_column :people, :login
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090730151856) do
+ActiveRecord::Schema.define(:version => 20090730173127) do
 
   create_table "mindapples", :force => true do |t|
     t.text     "suggestion"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(:version => 20090730151856) do
     t.string   "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "login",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
   end
 
 end
