fd26f7cc7ce3f0b1977894ef9341e0e840459252
Tom ten Thij <git@tomtenthij.nl>
2009-07-30 17:21:52 +0100

    Ensuring correct number of mindapples

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,19 @@ class Person < ActiveRecord::Base
   accepts_nested_attributes_for :mindapples
 
   def ensure_corrent_number_of_mindapples
-    5.times { mindapples.build }
+    chop_superfluous_mindapples
+    (5 - mindapples.size).times { mindapples.build }
+  end
+
+  private
+
+  def chop_superfluous_mindapples
+    return unless mindapples.size > 5
+    number_to_chop = mindapples.size - 5
+    number_to_chop.times { chop_one_mindapple }
+  end
+
+  def chop_one_mindapple
+    mindapples.delete(mindapples.first)
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -26,5 +26,19 @@ describe Person do
       person.ensure_corrent_number_of_mindapples
       person.should have(5).mindapples
     end
+
+    it "should assign only up to five mindapples if it already has some" do
+      person = Person.new
+      3.times { person.mindapples.build }
+      person.ensure_corrent_number_of_mindapples
+      person.should have(5).mindapples
+    end
+
+    it "should delete enough mindapples to have 5 left if it has more" do
+      person = Person.new
+      7.times { |i| person.mindapples.build(:created_at => i.days.ago, :suggestion => i.to_s) }
+      person.ensure_corrent_number_of_mindapples
+      person.should have(5).mindapples
+    end
   end
 end
