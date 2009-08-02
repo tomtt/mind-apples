6bdf87c89d787fca3914955e25e1b39f2da3e62d
Tom ten Thij <git@tomtenthij.nl>
2009-08-02 05:05:24 +0100

    specs for user sessions controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,23 +1,35 @@
 class UserSessionsController < ApplicationController
   resources_controller_for :user_sessions
-  before_filter :require_no_user, :only => [:new, :create]
+  # before_filter :require_no_user, :only => [:new, :create]
+  before_filter :logout_if_current_user, :only => [:create]
   before_filter :require_user, :only => :destroy
 
   response_for :create do |format|
     format.html do
       if @resource_saved
         flash[:notice] = "Login successful!"
-        redirect_back_or_default person_path(current_user)
+        redirect_back_or_default person_path(resource.person)
       else
         render :action => :new
       end
     end
   end
 
+  def destroy
+    @last_logged_in_user_id = current_user.to_param
+    current_user_session.destroy
+  end
+
   response_for :destroy do |format|
     format.html do
       flash[:notice] = "Logout successful!"
-      redirect_back_or_default new_user_session_path
+      redirect_back_or_default person_path(@last_logged_in_user_id)
     end
+  end
+
+  protected
+
+  def logout_if_current_user
+    current_user_session && current_user_session.destroy
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -9,17 +9,79 @@ describe UserSessionsController do
   end
 
   describe "create" do
+    before do
+      controller.stub!(:require_no_user)
+    end
+
+    it "should log the current logged in user out" do
+      @person = Factory.create(:person)
+      @mock_session = build_mock_session
+      @mock_session.stub!(:person).and_return @person
+      UserSession.stub!(:find).and_return(@mock_session)
+      @mock_session.should_receive(:destroy)
+      post(:create, "session" => {})
+    end
+
     describe "when saved" do
       before do
+        @person = Factory.create(:person)
         @mock_session = build_mock_session
         @mock_session.stub!(:save).and_return true
+        @mock_session.stub!(:person).and_return @person
+        @mock_session.stub!(:destroy)
         UserSession.stub!(:new).and_return(@mock_session)
+        UserSession.stub!(:find).and_return(@mock_session)
+      end
+
+      it "should set the flash notice with login successful" do
+        post(:create, "session" => {})
+        flash[:notice].should == "Login successful!"
       end
 
       it "should redirect back to account path redirect back is not set" do
         post(:create, "session" => {})
-        response.should redirect_to(person_path(current_user))
+        response.should redirect_to(person_path(@person))
       end
     end
+  end
+
+  describe "when not saved" do
+    before do
+      @mock_session = build_mock_session
+      @mock_session.stub!(:save).and_return false
+      UserSession.stub!(:new).and_return(@mock_session)
+    end
+
+    it "should render new" do
+      post(:create, "session" => {})
+      response.should render_template("new")
+    end
+  end
+
+  describe "destroy" do
+    before do
+      @person = Factory.create(:person)
+      @mock_session = build_mock_session
+      @mock_session.stub!(:destroy)
+      @mock_session.stub!(:person).and_return @person
+      UserSession.stub!(:find).and_return(@mock_session)
+      controller.stub!(:require_user)
+    end
+
+    it "should destroy the current user session" do
+      @mock_session.should_receive(:destroy)
+      delete :destroy, "session" => {}
+    end
+
+    it "should set the flash notice with logout successful" do
+      delete :destroy, "session" => {}
+      flash[:notice].should == "Logout successful!"
+    end
+
+    it "should redirect back to account path redirect back is not set" do
+      delete :destroy, "session" => {}
+      response.should redirect_to(person_path(@person))
+    end
+
   end
 end
