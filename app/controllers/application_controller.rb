79a45aaba3bd7285659d42edcd189e6d546d8fd3
Tom ten Thij <git@tomtenthij.nl>
2009-08-02 02:55:44 +0100

    Specs for and updates to the application controller (auth logic stuff)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,33 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.user
+    @current_user = current_user_session && current_user_session.person
+  end
+
+  def require_user
+    unless current_user
+      store_location
+      flash[:notice] = "You must be logged in to access this page"
+      redirect_to login_path
+      return false
+    end
+  end
+
+  def require_no_user
+    if current_user
+      store_location
+      flash[:notice] = "You must be logged out to access this page"
+      redirect_to root_path
+      return false
+    end
+  end
+
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,7 +4,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ApplicationController do
   # We can not call the application controller directly, use a dummy
   class DummiesController < ApplicationController
+    before_filter :require_no_user, :only => :something_only_for_logged_out_users
+    before_filter :require_user, :only => :something_private
+
     def index; render :text => 'foo'; end
+    def something_only_for_logged_out_users; render :text => 'hello'; end
+    def something_private; render :text => 'shh. secret'; end
     def authenticated
       current_user
       render :text => 'foo'
@@ -20,7 +25,7 @@ describe ApplicationController do
   end
 
   after(:all) do
-    load File.join(RAILS_ROOT, 'config', 'routes.rb')
+    ActionController::Routing::Routes.reload!
   end
 
   describe "current user session" do
@@ -30,24 +35,111 @@ describe ApplicationController do
     end
 
     it "should expose the current user session as @current_user_session" do
-      mock_user_session = mock_model(UserSession, :user => nil)
+      mock_user_session = mock_model(UserSession, :person => nil)
       UserSession.stub!(:find).and_return(mock_user_session)
       get 'authenticated'
       assigns[:current_user_session].should == mock_user_session
     end
 
     it "should find the current user" do
-      mock_user_session = mock_model(UserSession, :user => :mock_user)
+      mock_user_session = mock_model(UserSession, :person => :mock_user)
       UserSession.stub!(:find).and_return(mock_user_session)
-      mock_user_session.should_receive(:user)
+      mock_user_session.should_receive(:person)
       get 'authenticated'
     end
 
     it "should expose the current user as @current_user" do
-      mock_user_session = mock_model(UserSession, :user => :mock_user)
+      mock_user_session = mock_model(UserSession, :person => :mock_user)
       UserSession.stub!(:find).and_return(mock_user_session)
       get 'authenticated'
       assigns[:current_user].should == :mock_user
+    end
+  end
+
+  describe "require user" do
+    describe "when not logged in" do
+      before do
+        UserSession.stub!(:find).and_return nil
+      end
+
+      it "should store the current location" do
+        get 'something_private'
+        session[:return_to].should == request.request_uri
+      end
+
+      it "should store a flash stating you need to be logged in" do
+        get 'something_private'
+        flash[:notice].should =~ /must be logged in/
+      end
+
+      it "should redirect to the login path" do
+        get 'something_private'
+        response.should redirect_to(login_path)
+      end
+    end
+
+    describe "when logged in" do
+      before do
+        UserSession.stub!(:find).and_return mock_model(UserSession, :person => :mock_user)
+      end
+
+      it "should render 'shh. secret'" do
+        get 'something_private'
+        response.body.should have_text('shh. secret')
+      end
+    end
+  end
+
+  describe "require no user" do
+    describe "when not logged in" do
+      before do
+        UserSession.stub!(:find).and_return nil
+      end
+
+      it "should render 'hello'" do
+        get 'something_only_for_logged_out_users'
+        response.body.should have_text('hello')
+      end
+    end
+
+    describe "when logged in" do
+      before do
+        UserSession.stub!(:find).and_return mock_model(UserSession, :person => :mock_user)
+      end
+
+      it "should store the current location" do
+        get 'something_only_for_logged_out_users'
+        session[:return_to].should == request.request_uri
+      end
+
+      it "should store a flash stating you need to be logged out" do
+        get 'something_only_for_logged_out_users'
+        flash[:notice].should =~ /must be logged out/
+      end
+
+      it "should redirect to the login path" do
+        get 'something_only_for_logged_out_users'
+        response.should redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "redirect back or default" do
+    class DummiesController
+      def redirect_test
+        redirect_back_or_default(root_path)
+      end
+    end
+
+    it "should redirect to the return to value if set in the session" do
+      session[:return_to] = new_person_path
+      get 'redirect_test'
+      response.should redirect_to(new_person_path)
+    end
+
+    it "should redirect to the default value if none set in the session" do
+      get 'redirect_test'
+      response.should redirect_to(root_path)
     end
   end
 end
