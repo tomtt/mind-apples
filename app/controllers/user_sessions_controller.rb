9e2027ca7fbf6652774956ee33ca958856ca303d
Tom ten Thij <git@tomtenthij.nl>
2009-08-02 02:56:36 +0100

    User sessions controller specs

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     format.html do
       if @resource_saved
         flash[:notice] = "Login successful!"
-        redirect_back_or_default account_url
+        redirect_back_or_default person_path(current_user)
       else
         render :action => :new
       end
@@ -17,7 +17,7 @@ class UserSessionsController < ApplicationController
   response_for :destroy do |format|
     format.html do
       flash[:notice] = "Logout successful!"
-      redirect_back_or_default new_user_session_url
+      redirect_back_or_default new_user_session_path
     end
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,5 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UserSessionsController do
-  it "should have specs"
+  def build_mock_session
+    mock_model(UserSession,
+               :priority_record= => nil,
+               :persisting? => nil
+               )
+  end
+
+  describe "create" do
+    describe "when saved" do
+      before do
+        @mock_session = build_mock_session
+        @mock_session.stub!(:save).and_return true
+        UserSession.stub!(:new).and_return(@mock_session)
+      end
+
+      it "should redirect back to account path redirect back is not set" do
+        post(:create, "session" => {})
+        response.should redirect_to(person_path(current_user))
+      end
+    end
+  end
 end
