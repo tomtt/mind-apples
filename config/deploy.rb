d08e0c223390ff186ec7d4cccdb6562d5b217ac9
Tom ten Thij <git@tomtenthij.nl>
2009-09-28 18:08:50 +0100

    releases dir created on initial setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ namespace :deploy do
 
   after "deploy:setup", "deploy:initial_setup"
   task :initial_setup do
+    run "mkdir -p #{deploy_to}/releases"
     run "mkdir -p #{shared_path}/config"
     put(File.read(File.join(File.expand_path(File.dirname(__FILE__)), "database.yml.example")),
         "#{shared_path}/config/database.yml",
