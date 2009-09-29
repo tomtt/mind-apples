86b64933a2466db2abb6e8fc61fc3990f2f8cc42
Tom ten Thij <git@tomtenthij.nl>
2009-09-29 17:44:06 +0100

    Start with mailer for person

---
+++ app/models/person_mailer.rb
@@ -0,0 +1,21 @@
+class PersonMailer < ActionMailer::Base
+
+  def mindapples(sent_at = Time.now)
+    subject    'Your mindapples'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+
+    body       :greeting => 'Hi,'
+  end
+
+  def set_password(sent_at = Time.now)
+    subject    'Setting your Mindapples password'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+
+    body       :greeting => 'Hi,'
+  end
+
+end
