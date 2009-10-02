77cc1d4c6a6038a14473d604464f53fda540c2f6
Tom ten Thij <git@tomtenthij.nl>
2009-10-02 13:58:27 +0100

    Use spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,31 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
-require 'spec/rails'
+require 'spork'
 
-require File.expand_path(File.join(File.dirname(__FILE__), 'factories'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'shared_helper'))
+Spork.prefork do
+  ENV["RAILS_ENV"] = "test"
+  require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+  require 'spec'
+  require 'spec/rails'
 
-require "email_spec/helpers"
-require "email_spec/matchers"
+  require File.expand_path(File.join(File.dirname(__FILE__), 'factories'))
+  require File.expand_path(File.join(File.dirname(__FILE__), 'shared_helper'))
 
-Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  require "email_spec/helpers"
+  require "email_spec/matchers"
 
-  # config.include AuthenticatedTestHelper
+  Spec::Runner.configure do |config|
+    # If you're not using ActiveRecord you should remove these
+    # lines, delete config/database.yml and disable :active_record
+    # in your config/boot.rb
+    config.use_transactional_fixtures = true
+    config.use_instantiated_fixtures  = false
+    config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
-  config.include(EmailSpec::Helpers)
-  config.include(EmailSpec::Matchers)
+    # config.include AuthenticatedTestHelper
+
+    config.include(EmailSpec::Helpers)
+    config.include(EmailSpec::Matchers)
+  end
+end
+
+Spork.each_run do
 end
