c4ac7e0d2b5f1f28199d7b4b738839a05a96aa42
Tom ten Thij <git@tomtenthij.nl>
2009-10-12 13:40:37 +0100

    Sporkify cucumber env

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,22 +1,30 @@
-# Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] ||= "cucumber"
-require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require 'cucumber/rails/world'
+require 'spork'
 
-# Comment out the next line if you don't want Cucumber Unicode support
-require 'cucumber/formatter/unicode'
+Spork.prefork do
+  # Sets up the Rails environment for Cucumber
+  ENV["RAILS_ENV"] ||= "cucumber"
+  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+  require 'cucumber/rails/world'
 
-require 'webrat'
-require 'cucumber/webrat/table_locator' # Lets you do table.diff!(table_at('#my_table').to_a)
+  # Comment out the next line if you don't want Cucumber Unicode support
+  require 'cucumber/formatter/unicode'
 
-Webrat.configure do |config|
-  config.mode = :rails
+  require 'webrat'
+  require 'cucumber/webrat/table_locator' # Lets you do table.diff!(table_at('#my_table').to_a)
+
+  Webrat.configure do |config|
+    config.mode = :rails
+    config.open_error_files = false # Set to true if you want error pages to pop up in the browser
+  end
+
+  require 'cucumber/rails/rspec'
+  require 'webrat/core/matchers'
+
+  require File.join(RAILS_ROOT, "spec", "shared_helper")
+
+  require 'email_spec/cucumber'
 end
 
-require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
-
-require File.join(RAILS_ROOT, "spec", "factories")
-require File.join(RAILS_ROOT, "spec", "shared_helper")
-
-require 'email_spec/cucumber'
+Spork.each_run do
+  require File.join(RAILS_ROOT, "spec", "factories")
+end
