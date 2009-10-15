5f7a5cf8af6987d448d48d7ca2d67c90a72818dd
Tom ten Thij <git@tomtenthij.nl>
2009-10-15 16:20:43 +0100

    Cleanup of routes file, added password_resets resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+  # Resources
+  map.resource :user_session
+  map.resources :people, :as => "person", :except => [:index]
+  map.resources :password_resets
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
+  # Named routes
   map.root :controller => "pages", :action => "home"
   map.login '/login', :controller => "user_sessions", :action => "new"
   map.logout '/logout', :controller => "user_sessions", :action => "destroy"
 
-  map.resource :user_session
-  map.resources :people, :as => "person", :except => [:index]
+  # Individual pages
   map.about '/about', :controller => "pages", :action => "about"
   map.things_to_do '/things-to-do', :controller => "pages", :action => "things_to_do"
   map.help_us_grow '/help-us-grow', :controller => "pages", :action => "help_us_grow"
   map.about_us '/about-us', :controller => "pages", :action => "about_us"
   map.social_media_stuff '/social_media_stuff', :controller => "pages", :action => "social_media_stuff"
-
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
