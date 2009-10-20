8e0971cedf563918182b4ea967d5175c280922de
Tom ten Thij <git@tomtenthij.nl>
2009-10-20 14:36:52 +0100

    Open/close steps that have examples

--- public/javascripts/cucumber_step_completion.js
+++ public/javascripts/cucumber_step_completion.js
@@ -20,26 +20,45 @@ var cucumberStepCompletion = {
   stepMatchToHtml: function(elementOfArray, indexInArray) {
     return "<li>" + elementOfArray + "</li>";
   },
-  displayMatchingStepsFor: function(cucumberStep) {
-    var stepDefinition = cucumberStepCompletion.cachedSteps[cucumberStep];
+  matchingStepsToHtml: function(cucumberStepIndex) {
+    var stepDefinition = cucumberStepCompletion.cachedSteps[cucumberStepIndex];
     var matchingSteps = cucumberStepCompletion.cucumberSteps[stepDefinition];
     var matchingStepsItems = jQuery.map(matchingSteps, cucumberStepCompletion.stepMatchToHtml);
-    var html = "<b>" +  stepDefinition + " </b>(" + matchingStepsItems.length + ")\n<ul>";
+    var html = "<div class='matching-steps'><ul>";
     html += matchingStepsItems.join("\n");
-    html += "</ul>";
-    jQuery('.cuked-it-step-browser-matching-steps').html(html);
+    html += "</div></ul>";
+    return html;
+  },
+  displayMatchingStepsFor: function(cucumberStep) {
+    jQuery('.cuked-it-step-browser-matching-steps').html(cucumberStepCompletion.matchingStepsToHtml(cucumberStep));
   },
   stepTemplateToHtmlLi: function(elementOfArray, indexInArray) {
-    return "<li onMouseOver='cucumberStepCompletion.displayMatchingStepsFor(" + indexInArray + ");'>" + elementOfArray + "</li>\n";
+    return "<li>" + elementOfArray + "</li>\n";
   },
   stepTemplateToHtmlTr: function(elementOfArray, indexInArray) {
-    var mouseOver = "onMouseOver='cucumberStepCompletion.displayMatchingStepsFor(" + indexInArray + ");'";
     var count = cucumberStepCompletion.cucumberSteps[elementOfArray].length;
-    return "<tr " + mouseOver + "><td><div>" + elementOfArray + "</div></td><td>" + count + "</td></tr>\n";
+    var id = " id='step-definition-" + indexInArray + "'";
+    var onClick;
+    var class;
+
+    if (count > 0) {
+      onClick = " onClick='cucumberStepCompletion.toggleStepExamplesFor(" +
+        indexInArray + ");'"
+      class = " class='toggle closed step-definition'"
+    }
+    else {
+      onClick = "";
+      class = " class='step-definition'"
+    }
+
+    return "<tr><td><div" + class + onClick + id + ">" +
+      elementOfArray +
+      "</div></td><td class='number'>" + count + "</td></tr>\n";
+
   },
   selectFieldFromStepTemplates: function() {
     var html = "<select id='select-step' name='step'>";
-    varoptions =  jQuery.map(cucumberStepCompletion.stepTemplates(), 
+    varoptions =  jQuery.map(cucumberStepCompletion.stepTemplates(),
                              cucumberStepCompletion.stepTemplateToHtmlOption);
     html += options.join('');
     html += "</select>";
@@ -47,18 +66,19 @@ var cucumberStepCompletion = {
   },
   listOfStepTemplates: function() {
     var html = "<ul>";
-    var items = jQuery.map(cucumberStepCompletion.stepTemplates(), 
+    var items = jQuery.map(cucumberStepCompletion.stepTemplates(),
                            cucumberStepCompletion.stepTemplateToHtmlLi);
     html += items.join('');
     html += "</ul>";
     return html;
   },
   tableOfStepTemplates: function() {
-    var html = "<table><tr><th>Step definition</th><th>#</th></tr>\n";
-    var items = jQuery.map(cucumberStepCompletion.stepTemplates(), 
+    var html = "<table class='sorted cuked-it-step-definition-table'><thead><tr><th>Step definition</th><th>#&nbsp;&nbsp;&nbsp;</th></tr></thead>\n";
+    html += "<tbody>\n";
+    var items = jQuery.map(cucumberStepCompletion.stepTemplates(),
                            cucumberStepCompletion.stepTemplateToHtmlTr);
     html += items.join("\n");
-    html += "</table>";
+    html += "</tbody></table>";
     return html;
   },
   insertSelectField: function(divId) {
@@ -71,9 +91,22 @@ var cucumberStepCompletion = {
     var html = "<div class='cuked-it-step-browser-list'>";
     html += cucumberStepCompletion.htmlForStepBrowser();
     html += "</div>";
-    html += "<div class='cuked-it-step-browser-matching-steps'></div>";
     jQuery('.cuked-it-step-browser').html(html);
+  },
+  toggleStepExamplesFor: function(index) {
+    var isCurrentlyClosed = $('#step-definition-' + index).hasClass('closed');
+    var id = '#step-definition-' + index;
+    $(id).toggleClass('open').toggleClass('closed')
+    if (isCurrentlyClosed) {
+      jQuery(id).append(cucumberStepCompletion.matchingStepsToHtml(index));
+    }
+    else {
+      jQuery(id + " .matching-steps").empty();
+    }
   }
 }
 
 jQuery(document).ready(cucumberStepCompletion.createStepBrowser);
+jQuery(document).ready(function() {
+  jQuery('table.sorted').tablesorter();
+});
