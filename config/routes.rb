275ceca991050fdfad31629da142cf14c27609e1
Tom ten Thij <git@tomtenthij.nl>
2009-10-22 14:43:39 +0100

    Replace things to do page with (read the) fives page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Individual pages
   map.about '/about', :controller => "pages", :action => "about"
-  map.things_to_do '/things-to-do', :controller => "pages", :action => "things_to_do"
+  map.fives '/fives', :controller => "pages", :action => "fives"
   map.help_us_grow '/help-us-grow', :controller => "pages", :action => "help_us_grow"
   map.about_us '/about-us', :controller => "pages", :action => "about_us"
   map.social_media_stuff '/social_media_stuff', :controller => "pages", :action => "social_media_stuff"
