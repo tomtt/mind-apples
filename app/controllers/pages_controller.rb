a70d2dcee624d479738436fb5f18efdd8ebf7332
Andy Gibson <andy@sociability.org.uk>
2009-12-29 18:25:18 +0000

    Changed /home path and added new /home page file. Also made copy and layout changes to form and various apges.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
-  def home
-    redirect_to new_person_path
-  end
 
   def error
     raise 'Intentional error'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,5 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PagesController do
   describe "home" do
-    it "should redirect to the page to take the survey" do
-      get :home
-      response.should redirect_to(new_person_path)
-    end
   end
 end
