41136074d138eb866cef0a275eca94165ef24477
Tom ten Thij <git@tomtenthij.nl>
2010-01-01 03:02:57 +0000

    Step for showing response.body in a browser

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,3 +1,10 @@
 Then /^debugger$/ do
   debugger
 end
+
+Then "show me the page body in a rails context in a browser" do
+  File.open(File.join(RAILS_ROOT, 'public', "tmp.html"),"w") do |f|
+    f.puts response.body
+  end
+  system "open http://mindapples.local/tmp.html"
+end
