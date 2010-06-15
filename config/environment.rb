bd3d002e78b2913f7020497ce23ab84efd4351bb
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-06-15 13:04:51 +0100

    Add feedtools.

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
              :source => 'http://gems.github.com'
   config.gem 'mocha'
   config.gem 'pickle'
-
+  config.gem 'feedtools'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
