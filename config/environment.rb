abe34aa741c219cfc8305b493caade8e2874c049
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-06-15 13:20:03 +0100

    Add uuidtools for feedtools

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
   config.gem 'mocha'
   config.gem 'pickle'
   config.gem 'feedtools', :lib => 'feed_tools', :source => 'http://gems.github.com' 
+  config.gem 'uuidtools'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
