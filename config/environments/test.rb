ae6ff4eb28177b4ec0ef118f15fcb83f3665665a
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-06-15 12:49:23 +0100

    Fix fakeweb depency

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,7 +25,7 @@ config.action_mailer.default_url_options = {
   :host => 'mindapples.local'
 }
 config.action_mailer.delivery_method = :test
-
+config.gem 'fakeweb'
 # Use SQL instead of Active Record's schema dumper when creating the test database.
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
