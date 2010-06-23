bf5489f3ab25a9b578ba42495f339f002a61f139
Borja Arias <borja.arias@unboxedconsulting.com>
2010-06-23 11:52:47 +0100

    created model methods lo generate most_liked and most recent mindapples

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  
+  TOP_APPLES_MAX = 5
+  MOST_RECENT_APPLES_MAX = 5
+  
   def home
     @person = Person.new_with_mindapples
     @blogfeeds = BlogFeed.latest(3)
+    @most_liked = Mindapple.most_liked(TOP_APPLES_MAX)
+    @most_recent = Mindapple.most_recent(TOP_APPLES_MAX)    
   end
 
   def error
--- app/models/mindapple.rb
+++ app/models/mindapple.rb
@@ -12,4 +12,20 @@
 class Mindapple < ActiveRecord::Base
   belongs_to :person
   has_and_belongs_to_many :fans, :class_name => "Person"
+  
+  def self.most_liked(max)
+    Mindapple.find(:all, :select => "mindapple_id as id, suggestion, ma.created_at as created_at, count(*) as total",
+                         :joins => "as map inner join mindapples as ma on map.mindapple_id = ma.id",
+                         :from => "mindapples_people",
+                         :group => "mindapple_id",
+                         :order => "total DESC",
+                         :limit => max)
+  end
+  
+  def self.most_recent(max)
+    Mindapple.find(:all, :group => "person_id",
+                         :order => "created_at DESC",
+                         :limit => max)
+  end
+  
 end
