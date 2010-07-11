ccfdb37000ec5312576e871c3d6e40e1e8717b69
Tom ten Thij <git@tomtenthij.nl>
2010-07-11 02:18:48 +0100

    Code for email validation

---
+++ lib/email_validation.rb
@@ -0,0 +1,11 @@
+class EmailValidation
+  def self.valid?(email)
+    begin
+      address = TMail::Address.parse(email)
+      return false unless address.is_a?(TMail::Address)
+      !! ( address.domain =~ /\A([a-z0-9][a-z0-9-]*[a-z0-9]\.)+[a-z0-9]{2,}\Z/i )
+    rescue TMail::SyntaxError
+      return false
+    end
+  end
+end
---
+++ spec/lib/email_validation_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe EmailValidation do
+  describe "valid?" do
+    it "is true for valid email addresses" do
+      valid_addresses = ["bob@example.com"]
+      valid_addresses.each do |address|
+        EmailValidation.valid?(address).should be_true
+      end
+    end
+
+    it "is false for invalid email addresses" do
+      invalid_addresses = ["bob.builder", # no hostname
+                           "bob.builder@example.c-m", # weird char in hostname
+                           "bob:builder" # colon
+                          ]
+      invalid_addresses.each do |address|
+        EmailValidation.valid?(address).should be_false
+      end
+    end
+
+    it "should have more positive and negative examples, especially for the regexp that checks the domain"
+  end
+end
