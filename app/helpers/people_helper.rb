6621124ec46f1d185d733e356cdd8348072d50b4
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-07-12 16:55:48 +0100

    Add new condition for user name displaying on the mindapple.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,16 +1,16 @@
 module PeopleHelper
-  
+
   def populate_resource(person_resource)
     params[:person].each do |key, value| 
       person_resource[key.to_sym] = value unless value.blank? && key == 'login'
     end
   end
-  
+
   def person_link(mindapple)
-    return "anonymous" unless mindapple.person.login_set_by_user?
-    link_to (mindapple.person.name || mindapple.person.login), person_path(mindapple.person)
+    return (link_to (mindapple.person.name || mindapple.person.login), person_path(mindapple.person)) if mindapple.person.login_set_by_user?
+    (mindapple.person.name.blank?) ? "anonymous" : mindapple.person.name
   end
-  
+
   def suggestion(position)
     return '' unless session[:suggestions][position.to_s]
     person_suggestion = session[:suggestions][position.to_s]["suggestion"]
