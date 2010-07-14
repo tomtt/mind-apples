fb39da64212ec990a46ea2b876c63c23cb019325
Tom ten Thij <git@tomtenthij.nl>
2010-07-14 23:19:55 +0100

    Limit search to require at least 3 characters

--- app/controllers/mindapples_controller.rb
+++ app/controllers/mindapples_controller.rb
@@ -2,8 +2,12 @@ class MindapplesController < ApplicationController
 
   def index
     unless params['mindapple'].blank?
-      @mindapples = Mindapple.search_by_suggestion(params['mindapple']).paginate(:page => params[:page], :per_page => 10)
-      @search_message = "Sorry, it seems that we can't find what you are looking for." if @mindapples.size == 0
+      if params['mindapple'].size > 2
+        @mindapples = Mindapple.search_by_suggestion(params['mindapple']).paginate(:page => params[:page], :per_page => 10)
+        @search_message = "Sorry, it seems that we can't find what you are looking for." if @mindapples.size == 0
+      else
+        @search_message = "Sorry, can you give us a little more? 3 characters or more please."
+      end
     end
 
     respond_to do |format|
