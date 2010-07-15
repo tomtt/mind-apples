6d4a695eaa74e50ac5448683511514ead2602fd9
Tom ten Thij <git@tomtenthij.nl>
2010-07-15 09:20:57 +0100

    Use hoptoad to manage errors better

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   config.gem 'uuidtools'
   config.gem 'will_paginate'
   config.gem 'paperclip'
+  config.gem 'hoptoad_notifier'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '3674d67071649f5ee9f8abebe2cf593e'
+end
