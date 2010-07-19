741337e08c02313c2b9889cf67fbd6089805251d
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-07-19 15:16:13 +0100

    Add js event for cleaning password field.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,8 @@ function show_share_this_icons()
 { 
   document.getElementById("share_this_links").style.display="inline";
 }
+
+document.observe("dom:loaded", function() {
+  if($("person_password"))
+    $("person_password").value = ''
+});
