525d7fd8d239fdc27838043d0e7aeb7662944616
Andy Gibson <andy@sociability.org.uk>
2010-10-07 12:24:01 +0100

    Layout tweaks to the networks prior to launch

--- db/seeds/networks.rb
+++ db/seeds/networks.rb
@@ -9,13 +9,13 @@ module NetworkSeeds
   @@network_data << {
     "name" => "Lambeth",
     "url" => "lambeth",
-    "form_header" => "<img align='right' alt='Wellbeing and Health in Lambeth' src='/images/networks/lambeth_wellbeing_logo.jpg' />Mindapples is working with NHS Lambeth to find out what people in Lambeth need to be healthy and happy. Please share the 5-a-day for your mind below, and we'll share this data anonymously back with Lambeth to tell them what local people want and need to look after their minds."
+    "form_header" => "<img align='right' alt='Wellbeing and Health in Lambeth' src='/images/networks/lambeth_wellbeing_logo.jpg' />Mindapples is working with NHS Lambeth to find out what people in Lambeth need to be healthy and happy. <br /><br />Please share the 5-a-day for your mind below, and we'll share this data anonymously back with Lambeth to tell them what local people want and need to look after their minds."
   }
 
   @@network_data << {
     "name" => "4Beauty",
     "url" => "4beauty",
-    "form_header" => "<img align='right' alt='Mindapples and 4Beauty' src='/images/networks/4beauty_logo.jpg' />We want to find out what 4Beauty viewers want and need to be healthy and happy. Please share the 5-a-day for your mind below, and we'll share your data anonymously back with Channel 4 and tell them what you all want and need to look after your mind."
+    "form_header" => "<img align='right' alt='Mindapples and 4Beauty' src='/images/networks/4beauty_logo.jpg' />We want to find out what 4Beauty viewers want and need to be healthy and happy. <br /><br />Please share the 5-a-day for your mind below, and we'll share your data anonymously back with Channel 4 and tell them what you all want and need to look after your mind."
   }
 
   def self.seed_all
