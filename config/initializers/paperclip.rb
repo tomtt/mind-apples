0c3dd4d07faa6b0083836758b3422cec15be1d21
Tom ten Thij <git@tomtenthij.nl>
2011-02-18 12:59:22 +0000

    Allow the app to be started with S3 when run locally

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,9 @@
-if Rails.env.production?
+if Rails.env.production? || ENV['S3_ENABLED'] == "TRUE"
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:bucket] = ENV['S3_BUCKET']
   Paperclip::Attachment.default_options[:s3_credentials] = {
     :access_key_id => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
   }
+  Paperclip::Attachment.default_options[:path] = ":attachment/:id/:style.:extension"
 end
