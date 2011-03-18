c9ed0e300d63b9a022081acc87d5b1aac414f698
Tom ten Thij <git@tomtenthij.nl>
2011-03-18 10:52:20 +0000

    Code to perform the CSV import: not tested yet

--- app/models/people_import.rb
+++ app/models/people_import.rb
@@ -1,8 +1,53 @@
 require "aws/s3"
+require "fastercsv"
 
 class PeopleImport < ActiveRecord::Base
   BUCKET = "mindapples-people_imports"
   TOP_LEVEL_DIRECTORY = Rails.env
+
+  before_save :perform_csv_import
+  attr_reader :results
+
+  belongs_to :network
+
+  def perform_csv_import
+    s3_object = PeopleImport.find_csv_by_s3_key(s3_key)
+    csv_content = s3_object.value
+    data = []
+
+    FasterCSV.parse(csv_content) do |row|
+      data << row
+    end
+
+    keys = data.shift
+
+    @results = []
+
+    data.each do |values|
+      begin
+        attributes = ModelAttributes.construct(keys, values)
+        password = PageCode.code(20)
+        attributes["password"] = password
+        attributes["password_confirmation"] = password
+
+        begin
+          person = Person.new(attributes)
+          page_code = PageCode.code
+          person.page_code = page_code
+          person.login = '%s%s' % [Person::AUTOGEN_LOGIN_PREFIX, page_code]
+          person.network = network
+          person.type_description = user_type_description
+          person.import_s3_etag = s3_object.etag
+          person.save!
+          @results << person
+        rescue => e
+          @results << "#{attributes['email']} (#{attributes['name']}): Error: #{e.to_s}"
+        end
+      rescue => e
+        @results << "Parse error: #{e.to_s}"
+      end
+    end
+  end
 
   module S3ObjectHelpers
     def displayed_filename
