7ca5e69eff3c2a96ecad542221212d9bc72caaa7
Tom ten Thij <git@tomtenthij.nl>
2011-03-23 11:20:38 +0000

    Fix people import controller to correctly display error when something is wrong with the form

--- app/controllers/admin/people_imports_controller.rb
+++ app/controllers/admin/people_imports_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PeopleImportsController < Admin::AdminController
+  
   def index
     @csv_directory = PeopleImport.s3_csv_directory
     @csv_s3_objects = PeopleImport.s3_csv_objects
@@ -6,7 +7,7 @@ class Admin::PeopleImportsController < Admin::AdminController
 
   def new
     @csv_object = PeopleImport.find_csv_by_s3_key(params[:key])
-    @network_options = [["--- Select a network ---", nil]] + Network.all(:order => :name).map { |n| [n.name, n.id] }
+    assign_network_options
     @people_import = PeopleImport.new
   end
 
@@ -15,7 +16,15 @@ class Admin::PeopleImportsController < Admin::AdminController
     if @people_import.save
       flash[:notice] = "Import was completed."
     else
+      @csv_object = PeopleImport.find_csv_by_s3_key(@people_import.s3_key)
+      assign_network_options
       render :action => "new"
     end
+  end
+
+  private
+
+  def assign_network_options
+    @network_options = [["--- Select a network ---", nil]] + Network.all(:order => :name).map { |n| [n.name, n.id] }
   end
 end
