3b57f6783a60721ca7797a36261ae9d19c456572
Tom ten Thij <git@tomtenthij.nl>
2011-03-28 16:13:47 +0100

    Unset MIGRATING_TO_HEROKU constant having verified that it works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  MIGRATING_TO_HEROKU = true
+  MIGRATING_TO_HEROKU = false
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
