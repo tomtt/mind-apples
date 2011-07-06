5ab7a3261ce20eae3493c380e4eb5a7d1186d3d9
Ben Darlow <github@kapowaz.net>
2011-07-06 11:52:17 +0100

    Ignore .bundle/config, and added Rackup file for Pow

--- db/schema.rb
+++ db/schema.rb
@@ -69,12 +69,12 @@ ActiveRecord::Schema.define(:version => 20110327163728) do
     t.string   "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "login",                                        :null => false
-    t.string   "crypted_password",                             :null => false
-    t.string   "password_salt",                                :null => false
-    t.string   "persistence_token",                            :null => false
-    t.string   "single_access_token",                          :null => false
-    t.string   "perishable_token",                             :null => false
+    t.string   "login",                     :default => "",    :null => false
+    t.string   "crypted_password",          :default => "",    :null => false
+    t.string   "password_salt",             :default => "",    :null => false
+    t.string   "persistence_token",         :default => "",    :null => false
+    t.string   "single_access_token",       :default => "",    :null => false
+    t.string   "perishable_token",          :default => "",    :null => false
     t.integer  "login_count",               :default => 0,     :null => false
     t.integer  "failed_login_count",        :default => 0,     :null => false
     t.datetime "last_request_at"
