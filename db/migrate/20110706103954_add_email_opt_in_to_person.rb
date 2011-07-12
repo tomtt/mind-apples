f0ff49db70418c84cb13523a841d7bb97e6ffc7c
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-07-12 14:01:19 +0100

    Added default to migration for email-opt-in

--- db/migrate/20110706103954_add_email_opt_in_to_person.rb
+++ db/migrate/20110706103954_add_email_opt_in_to_person.rb
@@ -1,6 +1,6 @@
 class AddEmailOptInToPerson < ActiveRecord::Migration
   def self.up
-    add_column :people, :email_opt_in, :boolean
+    add_column :people, :email_opt_in, :boolean, :default => false
   end
 
   def self.down
