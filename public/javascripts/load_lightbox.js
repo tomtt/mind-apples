fcd849d04f0e7bad799678e1187bb4677b2475a1
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-07-13 14:24:10 +0100

    Adding lightbox when user opens their profile page.
    Needs to only show after mind apples creation
    Needs style

---
+++ public/javascripts/load_lightbox.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $(".take_the_test").append("<a href='/share' id='share_on_social_media'>Shere your mind apples with friends</a>");
+  $("#share_on_social_media").fancybox().trigger('click');
+});
