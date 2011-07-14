6499bcc915ba822234f2261c01b3971673bd063c
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-07-14 12:39:30 +0100

    Trying to fix authlogic-connect to correct credentials

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,14 @@
 class UserSession < Authlogic::Session::Base
   authenticate_with Person
   generalize_credentials_error_messages true
+  
+  def self.oauth_consumer
+    OAuth::Consumer.new(
+      "YH7ajAJOz65C8I5PVoGA", "R57Sxk6iDsEFfROcg17syrwnwWTwmltlxQeMdB7F5qo",
+      {
+        :site => "http://api.twitter.com",
+        :authorize_url => "https://api.twitter.com/oauth/authorize"
+      }
+    )
+  end
 end
