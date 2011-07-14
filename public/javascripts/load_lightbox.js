2af6cb16b9092e22cbf58a6ef02273fd3a51f998
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-07-14 10:57:41 +0100

    Remove load_lightbox file and append the javascript body to the only page that it's being used

--- public/javascripts/load_lightbox.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $("body").append("<a href='/share' id='share_on_social_media'>Shere your mind apples with friends</a>");
-  $("#share_on_social_media").fancybox().trigger('click');
-});
