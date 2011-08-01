6ef49bcd561e3f4e62c84111e51bbe51107d0e19
Leonardo Cassarani <leonardo.cassarani@unboxedconsulting.com>
2011-08-01 15:23:17 +0100

    Save attachments to an environment-specific S3 folder if we're not running in production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,14 @@
-if Rails.env.production? || ENV['S3_ENABLED'] == "TRUE"
+if Rails.env.production? || Rails.env.staging? || ENV['S3_ENABLED'] == "TRUE"
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:bucket] = ENV['S3_BUCKET']
   Paperclip::Attachment.default_options[:s3_credentials] = {
     :access_key_id => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
   }
-  Paperclip::Attachment.default_options[:path] = ":attachment/:id/:style.:extension"
+  
+  # Save attachments under an environment-specific directory to
+  # avoid uploading to the same paths as production
+  path = Rails.env.production? ? "" : "#{Rails.env}/"
+  path += ":attachment/:id/:style.:extension"
+  Paperclip::Attachment.default_options[:path] = path
 end
