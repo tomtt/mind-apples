557492b92ce9e6acfeb06b3eccf6dda8c0bc81f5
Attila Györffy <attila@attilagyorffy.com>
2011-12-20 11:57:28 +0000

    Refactor database migration a little for better understanding.

--- db/migrate/20111219132832_split_person_into_user_and_person.rb
+++ db/migrate/20111219132832_split_person_into_user_and_person.rb
@@ -1,5 +1,7 @@
 class SplitPersonIntoUserAndPerson < ActiveRecord::Migration
   
+  @fields = [:email, :login, :crypted_password, :password_salt, :persistence_token, :single_access_token, :perishable_token, :login_count, :failed_login_count, :last_request_at, :current_login_at, :last_login_at, :current_login_ip, :last_login_ip]
+  
   class MyPerson < ActiveRecord::Base
     set_table_name 'people'
     
@@ -13,7 +15,7 @@ class SplitPersonIntoUserAndPerson < ActiveRecord::Migration
     set_table_name 'users'
   end
   
-  def self.up
+  def self.create_users_table
     create_table :users do |t|
       t.string   "email",                     :null => false
       t.string   "login",                     :null => false
@@ -34,18 +36,29 @@ class SplitPersonIntoUserAndPerson < ActiveRecord::Migration
     end
     add_index :users, :login, :unique => true
     add_index :users, :email, :unique => true
-    
-    fields = [:email, :login, :crypted_password, :password_salt, :persistence_token, :single_access_token, :perishable_token, :login_count, :failed_login_count, :last_request_at, :current_login_at, :last_login_at, :current_login_ip, :last_login_ip]
-    
+  end
+  
+  def self.move_people_data_to_users
     MyPerson.find_each do |person|
       # Do not migrate people if they have been auto-generated
       next if person.anonymous?
       
       parameters = {}
-      fields.each { |field| parameters[field] = person.send(field) }
+      @fields.each { |field| parameters[field] = person.send(field) }
       MyUser.create!(parameters)
+
+      # PENDING: set up association between Person and User.
     end
-    
+  end
+  
+  def self.delete_redundant_columns_from_people
+    # TBC, we are going to have to keep email and login, though.
+  end
+  
+  def self.up
+    create_users_table
+    move_people_data_to_users
+    delete_redundant_columns_from_people
   end
 
   def self.down
