f8ca9763a0a7bc3ece412bb2f5c8163279299437
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2011-12-21 09:26:44 +0000

    Fix user sessions controller to work with new models

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,7 +12,11 @@ class UserSessionsController < ApplicationController
         if network = Network.find_by_id(params["network_id"])
           redirect_back_or_default network_path(network)
         else
-          redirect_back_or_default person_path(resource.person)
+          if current_user.person
+            redirect_back_or_default person_path(current_user.person)
+          else
+            redirect_back_or_default root_path
+          end
         end
       else
         if params[:network_id]
@@ -24,14 +28,22 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
-    @last_logged_in_user_id = current_user.to_param
+    @last_logged_in_person_param = current_user.person.try(:to_param)
     current_user_session.destroy
   end
 
   response_for :destroy do |format|
     format.html do
       flash[:notice] = "Logout successful!"
-      redirect_back_or_default person_path(@last_logged_in_user_id)
+      if network = Network.find_by_id(params["network_id"])
+        redirect_back_or_default network_path(network)
+      else
+        if @last_logged_in_person_param
+          redirect_back_or_default person_path(@last_logged_in_person_param)
+        else
+          redirect_back_or_default root_path
+        end
+      end
     end
   end
 
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -9,14 +9,15 @@ describe UserSessionsController do
   end
 
   describe "create" do
-    before do
-      controller.stubs(:require_no_user)
-    end
+    #before do
+      #controller.stubs(:require_no_user)
+    #end
 
     it "should log the current logged in user out" do
-      @person = Factory.create(:person)
+      @user = Factory.create(:user)
+      #@person = Factory.create(:person)
       @mock_session = build_mock_session
-      @mock_session.stubs(:person).returns @person
+      @mock_session.stubs(:record).returns @user
       UserSession.stubs(:find).returns(@mock_session)
       @mock_session.expects(:destroy)
       post(:create, "session" => {})
@@ -24,10 +25,11 @@ describe UserSessionsController do
 
     describe "when saved" do
       before do
-        @person = Factory.create(:person)
+        @user = Factory.create(:user)
+        #@person = Factory.create(:person)
         @mock_session = build_mock_session
         @mock_session.stubs(:save).returns true
-        @mock_session.stubs(:person).returns @person
+        @mock_session.stubs(:record).returns @user
         @mock_session.stubs(:destroy)
         UserSession.stubs(:new).returns(@mock_session)
         UserSession.stubs(:find).returns(@mock_session)
@@ -38,44 +40,50 @@ describe UserSessionsController do
         flash[:notice].should == "Login successful!"
       end
 
-      it "should redirect back to account path if redirect back is not set and no network was passed" do
+      it "should redirect back to the user's person path if they have a linked person and redirect back is not set and no network was passed" do
+        person = Factory.create(:person, :user => @user)
         post(:create, "session" => {})
-        response.should redirect_to(person_path(@person))
+        response.should redirect_to(person_path(person))
       end
 
-      it "should redirect back to network path if redirect back is not set and no network was passed" do
+      it "should redirect back to root if the uers has no person, and redirect back is not set" do
+        post(:create, "session" => {})
+        response.should redirect_to(root_path)
+      end
+
+      it "should redirect back to network path if redirect back is not set and a network was passed" do
         network = Factory.create(:network, :id => 999)
         post(:create, "session" => {}, "network_id" => "999")
         response.should redirect_to(network_path(network))
       end
     end
-  end
 
-  describe "when not saved" do
-    before do
-      @mock_session = build_mock_session
-      @mock_session.stubs(:save).returns false
-      UserSession.stubs(:new).returns(@mock_session)
-    end
+    describe "when not saved" do
+      before do
+        @mock_session = build_mock_session
+        @mock_session.stubs(:save).returns false
+        UserSession.stubs(:new).returns(@mock_session)
+      end
 
-    it "should render new" do
-      post(:create, "session" => {}, "network_id" => 777)
-      response.should render_template("new")
-    end
+      it "should render new" do
+        post(:create, "session" => {}, "network_id" => 777)
+        response.should render_template("new")
+      end
 
-    it "assigns to @network, the network that was passed as a param" do
-      Network.stubs(:find_by_id).with("777").returns(:mock_network)
-      post(:create, "session" => {}, "network_id" => 777)
-      assigns[:network].should == :mock_network
+      it "assigns to @network, the network that was passed as a param" do
+        Network.stubs(:find_by_id).with("777").returns(:mock_network)
+        post(:create, "session" => {}, "network_id" => 777)
+        assigns[:network].should == :mock_network
+      end
     end
   end
 
   describe "destroy" do
     before do
-      @person = Factory.create(:person)
+      @user = Factory.create(:user)
       @mock_session = build_mock_session
       @mock_session.stubs(:destroy)
-      @mock_session.stubs(:person).returns @person
+      @mock_session.stubs(:record).returns @user
       UserSession.stubs(:find).returns(@mock_session)
       controller.stubs(:require_user)
     end
@@ -90,10 +98,26 @@ describe UserSessionsController do
       flash[:notice].should == "Logout successful!"
     end
 
-    it "should redirect back to account path redirect back is not set" do
+    #it "should redirect back to account path redirect back is not set" do
+      #delete :destroy, "session" => {}
+      #response.should redirect_to(person_path(@person))
+    #end
+
+    it "should redirect back to the user's person path if they have a linked person and redirect back is not set" do
+      person = Factory.create(:person, :user => @user)
       delete :destroy, "session" => {}
-      response.should redirect_to(person_path(@person))
+      response.should redirect_to(person_path(person))
     end
 
+    it "should redirect back to root if the uers has no person, and redirect back is not set" do
+      delete :destroy, "session" => {}
+      response.should redirect_to(root_path)
+    end
+
+    it "should redirect back to network path if redirect back is not set and a network was passed" do
+      network = Factory.create(:network, :id => 999)
+      delete :destroy, "session" => {}, "network_id" => "999"
+      response.should redirect_to(network_path(network))
+    end
   end
 end
