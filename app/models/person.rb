3e5df7022555a61c88662a4e3947f5bc253c713a
Attila Györffy <attila@attilagyorffy.com>
2011-12-22 15:04:04 +0000

    Annotate Person.

--- app/models/person.rb
+++ app/models/person.rb
@@ -15,12 +15,12 @@
 #  tags                      :string(255)
 #  created_at                :datetime
 #  updated_at                :datetime
-#  login                     :string(255)     default(""), not null
-#  crypted_password          :string(255)     default(""), not null
-#  password_salt             :string(255)     default(""), not null
-#  persistence_token         :string(255)     default(""), not null
-#  single_access_token       :string(255)     default(""), not null
-#  perishable_token          :string(255)     default(""), not null
+#  login                     :string(255)
+#  crypted_password          :string(255)     not null
+#  password_salt             :string(255)     not null
+#  persistence_token         :string(255)     not null
+#  single_access_token       :string(255)     not null
+#  perishable_token          :string(255)     not null
 #  login_count               :integer         default(0), not null
 #  failed_login_count        :integer         default(0), not null
 #  last_request_at           :datetime
