3419ba52678992436df1bbd616f27268dedd96fb
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2012-01-03 10:33:11 +0000

    Fix person_helper to work with new model structure

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -7,18 +7,19 @@ module PeopleHelper
   end
 
   def person_link(mindapple)
-    if mindapple.person.anonymous? && ( mindapple.person.public_profile == true || match_logger_user?( mindapple.person))
-      name = mindapple.person.name || mindapple.person.login
-      link_to name, person_path(mindapple.person)
-    elsif mindapple.person.name.blank? || mindapple.person.public_profile == false
-      "anonymous"
+    person = mindapple.person
+    if ! person.anonymous? and (person.public_profile or current_user_owns_person?(person))
+      name = person.name || person.user.login
+      link_to name, person_path(person)
+    elsif person.public_profile and person.name.present?
+      person.name
     else
-      mindapple.person.name
+      "anonymous"
     end
   end
 
-  def match_logger_user?(person)
-    current_user.id == person.id if current_user
+  def current_user_owns_person?(person)
+    !! current_user and person.user == current_user
   end
 
   def suggestion(position)
