a737d37b930dd4b15e079223088c5b31d72433be
Lee Richmond <lee.richmond@unboxedconsulting.com>
2012-02-09 17:18:36 +0000

    added initializer for omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,25 @@
+require 'openid/store/filesystem'
+
+ActionController::Dispatcher.middleware.use OmniAuth::Builder do
+  # :scope parameter specifies the requested permissions for graph api. if you set it to empty, it will only pull the basic info. Default values for this is 'email, offline_access'
+  # provide Site URL as http://localhost:3000/ in your facebook app. Now you will be able to access it from your development machine
+  #  facebook_options = {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}, :scope => ''}
+
+  provider :twitter, 'YH7ajAJOz65C8I5PVoGA', 'R57Sxk6iDsEFfROcg17syrwnwWTwmltlxQeMdB7F5qo'
+  provider :facebook, 'APP_ID', 'APP_SECRET' #, facebook_options
+
+  # provider :open_id,  OpenID::Store::Filesystem.new('/tmp')
+end
+
+# you will be able to access the above providers by the following url
+# /auth/providername for example /auth/twitter /auth/facebook
+
+ActionController::Dispatcher.middleware do
+  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "google",  :identifier => "https://www.google.com/accounts/o8/id"
+end
+
+# you won't be able to access the openid urls like /auth/google
+# you will be able to access them through
+# /auth/open_id?openid_url=https://www.google.com/accounts/o8/id
+# /auth/open_id?openid_url=https://me.yahoo.com
+
