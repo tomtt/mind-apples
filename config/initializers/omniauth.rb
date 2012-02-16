81097bbaf57a3223d7f1f79f7622349a46d65625
Lee Richmond <lee.richmond@unboxedconsulting.com>
2012-02-16 17:45:35 +0000

    testing using the fb credentials directly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ ActionController::Dispatcher.middleware.use OmniAuth::Builder do
  
   provider :twitter, 'YH7ajAJOz65C8I5PVoGA', 'R57Sxk6iDsEFfROcg17syrwnwWTwmltlxQeMdB7F5qo'
 #  provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret']
-  provider :facebook '149521741834368', '56c6eba1e8effa37c8e38c7d3fa6a5f7'
+  provider :facebook, '149521741834368', '56c6eba1e8effa37c8e38c7d3fa6a5f7'
 end
 
 # you will be able to access the above providers by the following url
