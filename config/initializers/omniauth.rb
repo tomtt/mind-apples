62325a03910a988ed0ddd4a29e6b1919429b7f9b
Lee Richmond <lee.richmond@unboxedconsulting.com>
2012-02-17 13:16:13 +0000

    added config for ssl on facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,14 @@
 require 'openid/store/filesystem'
 
 FACEBOOK = YAML.load_file("#{RAILS_ROOT}/config/facebook.yml")[RAILS_ENV]
+
 ActionController::Dispatcher.middleware.use OmniAuth::Builder do
  
   provider :twitter, 'YH7ajAJOz65C8I5PVoGA', 'R57Sxk6iDsEFfROcg17syrwnwWTwmltlxQeMdB7F5qo'
-#  provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret']
-  provider :facebook, '149521741834368', '56c6eba1e8effa37c8e38c7d3fa6a5f7'
+  if RAILS_ENV == "development"
+    provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret']
+  else
+    provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
 
 # you will be able to access the above providers by the following url
